a40e1377988af3f1df57c81115a3b348de1787a4
Fareza Dava Rabbani <farezadavarabbani@gmail.com>
2025-01-01 14:31:48 +0700

    dasbord tamplate

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,11 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('Login/v_login.php');
+        //return view('Login/v_login.php');
+        //return view('admin/dashboard.php');
+        //return view('dokter/dashboard.php');
+        //return view('laboratorium/dashboard.php');
+        //return view('radiologi/dashboard.php');
+        return view('loket/dashboard.php');
     }
 }
