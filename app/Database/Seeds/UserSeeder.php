b310e1fb7b9f8c0fb6ced25ca75a1e72d6953ab7
Fareza Dava Rabbani <farezadavarabbani@gmail.com>
2025-01-09 17:17:56 +0700

    Update UserSeeder.php

--- app/Database/Seeds/UserSeeder.php
+++ app/Database/Seeds/UserSeeder.php
@@ -8,26 +8,75 @@ class UserSeeder extends Seeder
     public function run()
     {
         $data = [
-            //[
-            //    'nama_lengkap' => 'Fareza Dava Rabbani',
-            //    'username' => 'admin',
-            //    'password' => password_hash('admin123', PASSWORD_DEFAULT),
-            //    'email' => 'farezadavarabbani@gmail.com',
-            //    'role_id' => 1, // admin_sirs
-            //    'last_login' => date('Y-m-d H:i:s'),
-            //    'created_at' => date('Y-m-d H:i:s'), // Tambahkan created_at
-            //    'updated_at' => date('Y-m-d H:i:s'), // Tambahkan updated_at
-            //],
-            // Tambahkan lebih banyak pengguna jika perlu
             [
-                'nama_lengkap' => 'Saya user laboratorium',
-                'username' => 'user_radiologi',
-                'password' => password_hash('user123', PASSWORD_DEFAULT),
-                'email' => 'userradiologi@gmail.com',
-                'role_id' => 6, // admin_sirs
+                'nama_lengkap' => 'Admin SIRS',
+                'username' => 'admin_sirs',
+                'password' => password_hash('admin123', PASSWORD_DEFAULT),
+                'email' => 'admin_sirs@gmail.com',
+                'role_id' => 1, // admin_sirs
                 'last_login' => date('Y-m-d H:i:s'),
-                'created_at' => date('Y-m-d H:i:s'), // Tambahkan created_at
-                'updated_at' => date('Y-m-d H:i:s'), // Tambahkan updated_at
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'nama_lengkap' => 'Loket User',
+                'username' => 'user_loket',
+                'password' => password_hash('loket123', PASSWORD_DEFAULT),
+                'email' => 'user_loket@gmail.com',
+                'role_id' => 2, // loket
+                'last_login' => date('Y-m-d H:i:s'),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'nama_lengkap' => 'Dokter User',
+                'username' => 'user_dokter',
+                'password' => password_hash('dokter123', PASSWORD_DEFAULT),
+                'email' => 'user_dokter@gmail.com',
+                'role_id' => 3, // dokter
+                'last_login' => date('Y-m-d H:i:s'),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'nama_lengkap' => 'User Laboratorium',
+                'username' => 'user_lab',
+                'password' => password_hash('lab123', PASSWORD_DEFAULT),
+                'email' => 'user_lab@gmail.com',
+                'role_id' => 4, // user_lab
+                'last_login' => date('Y-m-d H:i:s'),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'nama_lengkap' => 'Admin Laboratorium',
+                'username' => 'admin_lab',
+                'password' => password_hash('adminlab123', PASSWORD_DEFAULT),
+                'email' => 'admin_lab@gmail.com',
+                'role_id' => 5, // admin_lab
+                'last_login' => date('Y-m-d H:i:s'),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'nama_lengkap' => 'User Radiologi',
+                'username' => 'user_rad',
+                'password' => password_hash('rad123', PASSWORD_DEFAULT),
+                'email' => 'user_rad@gmail.com',
+                'role_id' => 6, // user_rad
+                'last_login' => date('Y-m-d H:i:s'),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'nama_lengkap' => 'Admin Radiologi',
+                'username' => 'admin_rad',
+                'password' => password_hash('adminrad123', PASSWORD_DEFAULT),
+                'email' => 'admin_rad@gmail.com',
+                'role_id' => 7, // admin_rad
+                'last_login' => date('Y-m-d H:i:s'),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
             ],
         ];
 
